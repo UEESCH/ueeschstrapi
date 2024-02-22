42fa095bdd4f523b416c89686f2a2c19c2e7093c
UEESCH <santyago3525@outlook.com>
2024-02-22 16:43:34 -0500

    nuevos cambios

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
+const crypto = require('crypto');
 module.exports = ({ env }) => ({
-    // ...
+    // ..."
+    'users-permissions': {
+      config: {
+        jwtSecret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
+      },
+    },
+
+    
     upload: {
       config: {
         provider: 'cloudinary',
